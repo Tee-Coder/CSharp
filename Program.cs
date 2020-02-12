7325b9d88c851fa2337ddb71e5677209acea6c54
Toyin <oluwatoyinogundare@gmail.com>
2020-02-12 14:10:26 -0700

    added web files

--- Program.cs
+++ Program.cs
@@ -5,7 +5,41 @@ namespace CSharpExample
     class Program
     {
         static void Main(string[] args)
+
        {
+           int [] myarray = new int[5] {2,4,6,5,2};
+           myarray[1] = 2;
+           Console.WriteLine(myarray[0]);
+           Console.WriteLine(myarray[1]);
+           Console.WriteLine( myarray[3]); 
+
+foreach (int e in myarray)
+{
+    Console.WriteLine (e);
+}
+{
+    
+}
+
+
+
+            
+                int [] myarray1 = new int[4]{3,5,7,9};
+                
+                for (int m = 0; m < 4; m++)
+            
+                {
+                    Console.WriteLine(myarray1[m]);       
+                }
+
+                //Lets collect users input
+
+                Console.WriteLine("Please enter your name");
+                string myname = Console.ReadLine();
+                Console.WriteLine("Thank you," + myname);
+
+
+
            
            int x = 30;
             Console.WriteLine("Hello Toyin" + x);
@@ -44,9 +78,17 @@ namespace CSharpExample
                     Console.WriteLine("Uh oh, it is another value!");
                     break;
 
+
+
                }
+               
+                    MyMethod();
        }
 
-        
+       static void MyMethod()
+       {
+
+           Console.WriteLine("Test!");
+       }
     }
 }
